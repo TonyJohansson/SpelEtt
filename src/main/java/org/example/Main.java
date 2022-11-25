a5ff195f7f85e6dc11f1b2a10c99d02165339e57
TonyJohansson <tony.johansson.it@outlook.com>
2022-11-25 14:59:36 +0100

    tog bort lite skit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,15 @@
 package org.example;
 
-import com.googlecode.lanterna.TextColor;
 import com.googlecode.lanterna.input.KeyStroke;
 import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
-
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
-
 import static com.googlecode.lanterna.TextColor.ANSI.*;
-import static java.awt.Color.ORANGE;
+
 
 public class Main  {
     static DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory();
